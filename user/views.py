d031369f7caf03dc4cdf8f83da6d6121b80330f3
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-12 15:40:37 +0900

    Add: 회원가입 기능 추가

---
+++ user/views.py
@@ -0,0 +1,41 @@
+import json
+import bcrypt
+import jwt
+
+from django.http    import JsonResponse
+from django.views   import View
+
+from wikea.settings import SECRET_KEY
+from my_settings    import ALGORITHM
+from user.models    import User
+
+class SignupView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        try:
+            first_name      = data['first_name']
+            last_name       = data['last_name']
+            email           = data['email']
+            birthday        = data['birthday']
+            phone_number    = data['phone_number']
+            hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'),bcrypt.gensalt()).decode('utf-8')
+            
+
+            if User.objects.filter(first_name = first_name, last_name = last_name).exists():
+                return JsonResponse({'message':'DUPLICATE NAME'}, status=409)
+            if User.objects.filter(email = email).exists():
+                return JsonResponse({'message':'DUPLICATE EMAIL'}, status=409)
+            if User.objects.filter(phone_number = phone_number).exists():
+                return JsonResponse({'message':'DUPLICATE PHONE_NUMBER'}, status=409)
+            
+            User.objects.create(
+                first_name   = first_name,
+                last_name    = last_name,
+                email        = email,
+                birthday     = birthday,
+                phone_number = phone_number,
+                password     = hashed_password
+            )
+            return JsonResponse({'message':'SUCCESS'}, status=200)
+        except KeyError:
+            return JsonResponse({'message':'KEY ERROR'}, status=400)
