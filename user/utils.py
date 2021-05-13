7086643c3b196e92b263ae09e425a1df7cf91b36
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-13 13:37:28 +0900

    Add: Token Authentication in user/utils.py

---
+++ user/utils.py
@@ -0,0 +1,21 @@
+import jwt
+
+from django.http  import HttpResponse, JsonResponse
+
+from user.models import User
+from wikea.settings import SECRET_KEY
+
+def authorize(func):
+    
+    def wrapper():
+        if not access_token:
+            return JsonResponse({'MESSAGE': 'NECCESSARY LOGIN'}, status=403)
+
+        header = jwt.decode(access_token, SECRET_KEY, algorithms='HS256')
+
+        if not User.objects.filter(id = header['user_id']).exists():
+            return JsonResponse({'MESSAGE': 'UNAUTHORIZED USER'}, status=403)
+        else:
+            func()
+
+    return wrapper()
--- user/views.py
+++ user/views.py
@@ -1,9 +1,9 @@
 import json, bcrypt, jwt
 
 from django.views import View
-from django.http  import HttpResponse, JsonResponse
-from user.models  import User
+from django.http  import JsonResponse
 
+from user.models  import User
 from wikea.settings import SECRET_KEY
 
 class LogIn(View):
@@ -14,17 +14,17 @@ class LogIn(View):
             log_password = data['password'].encode('utf-8')
 
             if not User.objects.filter(email=log_email).exists():
-                return JsonResponse({"MESSAGE": "INVALID_USER"}, status=401)
+                return JsonResponse({'MESSAGE': 'INVALID_USER'}, status=401)
 
             user            = User.objects.get(email=log_email)
             user_password   = user.password.encode('utf-8')
 
             if not bcrypt.checkpw(log_password, user_password):
-                return JsonResponse({"MASSAGE": "INVALID_USER"}, status=401)
+                return JsonResponse({'MASSAGE': 'INVALID_USER'}, status=401)
 
             data         = {'user_id':user.id}
             access_token = jwt.encode(data, SECRET_KEY, algorithm='HS256')   
-            return JsonResponse({'token':access_token}, status=200)
+            return JsonResponse({'token':access_token, 'user_id':jwt.decode(access_token, SECRET_KEY, algorithms='HS256')}, status=200)
 
         except KeyError:
             return JsonResponse({'MASSAGE':'KEYERROR'}, status=401)
