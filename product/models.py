effc02a8d5c595b4b1f283a2d97748c12d17e2b9
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-13 12:07:48 +0900

    Refactor: models

--- product/models.py
+++ product/models.py
@@ -2,8 +2,8 @@ from django.db              import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Product(models.Model):
-    ko_name   = models.CharField(max_length=128)
-    en_name  = models.CharField(max_length=128)
+    korean_name   = models.CharField(max_length=128)
+    english_name  = models.CharField(max_length=128)
     stock         = models.IntegerField(default=0)
     price         = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     special_price = models.DecimalField(max_digits=10, decimal_places=2, default=None)
@@ -18,39 +18,39 @@ class Product(models.Model):
         db_table = "products"
 
 class Series(models.Model):
-    ko_name   = models.CharField(max_length=64)
-    en_name  = models.CharField(max_length=64)
+    korean_name  = models.CharField(max_length=64)
+    english_name = models.CharField(max_length=64)
 
     class Meta:
         db_table = "series"
 
 class Category(models.Model):
-    ko_name   = models.CharField(max_length=64)
-    en_name  = models.CharField(max_length=64)
+    korean_name  = models.CharField(max_length=64)
+    english_name = models.CharField(max_length=64)
 
     class Meta:
         db_table = "category"
 
 class SubCategory(models.Model):
-    ko_name   = models.CharField(max_length=64)
-    en_name  = models.CharField(max_length=64)
+    korean_name  = models.CharField(max_length=64)
+    english_name = models.CharField(max_length=64)
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
     
     class Meta:
         db_table = "sub_category"
 
 class Comment(models.Model):
-    content    = models.TextField(blank=True, null=True)
-    user       = models.ForeignKey("user.User", on_delete=models.CASCADE, related_name="user") 
-    product    = models.ForeignKey("Product", on_delete=models.CASCADE, related_name="product")
-    rating     = models.PositiveIntegerField(validators=[MinValueValidator(0),MaxValueValidator(5)], default=0)
+    content = models.TextField(blank=True, null=True)
+    user    = models.ForeignKey("user.User", on_delete=models.CASCADE, related_name="user") 
+    product = models.ForeignKey("Product", on_delete=models.CASCADE, related_name="product")
+    rating  = models.PositiveIntegerField(validators=[MinValueValidator(0),MaxValueValidator(5)], default=0)
 
     class Meta:
         db_table = "comments"
 
 class Color(models.Model):
-    ko_name  = models.CharField(max_length=32)
-    en_name  = models.CharField(max_length=32)
+    korean_name  = models.CharField(max_length=32)
+    english_name = models.CharField(max_length=32)
     
     class Meta:
         db_table = "colors"
