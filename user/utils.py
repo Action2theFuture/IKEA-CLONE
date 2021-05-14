d0825987d5052c7141b3951fdc91490b57d8ec96
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-14 10:56:25 +0900

    Refactor: 키에러 추가, pay_load 'key' 수정

--- user/utils.py
+++ user/utils.py
@@ -7,19 +7,21 @@ from user.models    import User
 from wikea.settings import SECRET_KEY
 
 def authorize(func):
-    
     def wrapper(self, request, *args, **kwargs):
         try:
             access_token = request.headers.get('Authorization', None)
             payload      = jwt.decode(access_token, SECRET_KEY, algorithms='HS256')
-            user         = User.objects.get(id=payload['id'])
+            user         = User.objects.get(id=payload['user_id'])
             request.user = user
         
         except jwt.exceptions.DecodeError:
-            return JsonResponse({'message' : 'INVALID_TOKEN' }, status=400)
+            return JsonResponse({'message':'INVALID_TOKEN' }, status=400)
 
         except Account.DoesNotExist:
-            return JsonResponse({'message' : 'INVALID_USER'}, status=400)
+            return JsonResponse({'message':'INVALID_USER'}, status=400)
+        
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
 
         return func(self, request, *args, **kwargs)
 
