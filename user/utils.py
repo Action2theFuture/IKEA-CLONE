663ea050488774ca41db8d54a9be1284a1d99fac
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-14 14:44:15 +0900

    Refactor: KeyError 추가

--- user/utils.py
+++ user/utils.py
@@ -9,20 +9,19 @@ from wikea.settings import SECRET_KEY
 def authorize(func):
     def wrapper(self, request, *args, **kwargs):
         try:
-            access_token = request.headers.get('Authorization', None)
+            access_token = request.headers['Authorization']
             payload      = jwt.decode(access_token, SECRET_KEY, algorithms='HS256')
             user         = User.objects.get(id=payload['user_id'])
             request.user = user
-        
-            if not access_token:
-                return JsonResponse({'message':'KEY_ERROR'}, status=400)
+
+            return func(self, request, *args, **kwargs)
+            
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
 
         except jwt.exceptions.DecodeError:
             return JsonResponse({'message':'INVALID_TOKEN' }, status=400)
 
         except Account.DoesNotExist:
             return JsonResponse({'message':'INVALID_USER'}, status=400)
-
-        return func(self, request, *args, **kwargs)
-
     return wrapper()
