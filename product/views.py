043e8545f134e0bd65226279a34a70e759b215a3
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-16 18:28:19 +0900

    Refactor: MainView json데이터 수정

--- product/views.py
+++ product/views.py
@@ -60,46 +60,59 @@ class MainView(View):
             right_image_bed     = Product.objects.get(english_name="adidasbed")
             right_image_storage = Product.objects.get(english_name="adidasstorage")
             new_products = [
-                {
-                    'lamp' : {
+                [             
+                    {
+                        'id'          : left_image_lamp.id,
                         'is_new'      : left_image_lamp.is_new,
                         'english_name': left_image_lamp.english_name,
                         'korean_name' : left_image_lamp.korean_name,
+                        'sub_category': left_image_lamp.sub_category.korean_name,
                         'price'       : left_image_lamp.price
                     },
-                    'bed' : {
+                    {
+                        'id'          : left_image_bed.id,
                         'is_new'      : left_image_bed.is_new,
                         'english_name': left_image_bed.english_name,
                         'korean_name' : left_image_bed.korean_name,
+                        'sub_category': left_image_bed.sub_category.korean_name,
                         'price'       : left_image_bed.price
                     },
-                    'storage' : {
+                    {
+                        'id'          : left_image_storage.id,
                         'is_new'      : left_image_storage.is_new,
                         'english_name': left_image_storage.english_name,
                         'korean_name' : left_image_storage.korean_name,
+                        'sub_category': left_image_storage.sub_category.korean_name,
                         'price'       : left_image_storage.price
                     }
-                },
-                {
-                    'lamp' : {
+                ]
+                ,
+                [
+                    {
+                        'id'          : right_image_lamp.id,
                         'is_new'      : right_image_lamp.is_new,
                         'english_name': right_image_lamp.english_name,
                         'korean_name' : right_image_lamp.korean_name,
+                        'sub_category': right_image_lamp.sub_category.korean_name,
                         'price'       : right_image_lamp.price
                     },
-                    'bed' : {
+                    {
+                        'id'          : right_image_bed.id,
                         'is_new'      : right_image_bed.is_new,
                         'english_name': right_image_bed.english_name,
                         'korean_name' : right_image_bed.korean_name,
+                        'sub_category': right_image_bed.sub_category.korean_name,
                         'price'       : right_image_bed.price
                     },
-                    'storage' : {
+                    {
+                        'id'          : right_image_storage.id,
                         'is_new'      : right_image_storage.is_new,
                         'english_name': right_image_storage.english_name,
                         'korean_name' : right_image_storage.korean_name,
+                        'sub_category': right_image_storage.sub_category.korean_name,
                         'price'       : right_image_storage.price
                     }
-                }
+                ]
             ]
             
             return JsonResponse({'category_list':category_list,'sub_category_list':sub_category_list,'recommended':recommend_product, 'new_products':new_products}, status=200)
