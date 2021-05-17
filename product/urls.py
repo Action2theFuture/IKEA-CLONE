76093ea89dc9c8d1794d32c6fd35ed0a6d914e7c
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-17 16:59:21 +0900

    Refactor: conflict

--- product/urls.py
+++ product/urls.py
@@ -3,6 +3,5 @@ from django.urls   import path
 from product.views import Main, Category
 
 urlpatterns = [
-   path('/category', Category.as_view()),
-   path('/main', Main.as_view()), 
+   path('/category', Category.as_view()), 
 ]
