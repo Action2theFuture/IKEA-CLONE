bbdfb0503c1e27f77682f25b2614102e1ecb5590
Action2theFuture <dnstks0204@google.com>
2021-05-17 17:10:31 +0900

    detail view

--- product/views.py
+++ product/views.py
@@ -12,8 +12,8 @@ class ProductDetailView(View):
             product      = Product.objects.filter(english_name=product_name).values()
             product_id   = Product.objects.get(english_name=product_name)
             descriptions = Description.objects.filter(product=product_id).values()
-            #color_list  = [color.name for color in product.color.all()]
-            #images      = Image.objects.filter(product=product).url
+            color_list   = [color.name for color in product.color.all()]
+            images       = Image.objects.filter(product=product).url
 
             random_number         = random.randrange(1,SubCategory.objects.count()+1)
             recommend_subcategory = SubCategory.objects.get(id=random_number)
@@ -28,14 +28,14 @@ class ProductDetailView(View):
                     'price'       : product_id.price,
                     'stock'       : product_id.stock,
                     'is_new'      : product_id.is_new,
-                    #'url'         : list(images)
+                    'url'         : list(images)
                     'descriptions': list(descriptions),
                 },
             )
 
             return JsonResponse({'product': product_list,
             'recommend_list': list(recommend_products)
-            #'color':color_list, 
-            #'images':list(images)
+            'color':color_list, 
+            'images':list(images)
             }, status=200)
         return JsonResponse({'MASSAGE':'Non-existent Product'}, status=404)
