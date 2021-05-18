87fc9ab839e9ed264cf7a2383c204e2efe1a59fb
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-18 21:25:15 +0900

    commit

--- order/views.py
+++ order/views.py
@@ -24,15 +24,16 @@ class OrderListView(View):
         total_order_price = 0
         for order in order_list: 
             order_product = OrderList.objects.get(order=order).product
-            order_products.append(
-                {   
-                    order_product.id,
-                    order_product.english_name,
-                    order_product.korean_name,
-                    order_product.sub_category,
-                    order_product.price
+            order_products = [
+                {
+                    'id'          : order_product.id,
+                    'english_name': order_product.english_name,
+                    'korean_name' : order_product.korean_name,
+                    'sub_category': order_product.sub_category,
+                    'price'       : order_product.price
                 }
-            )
+                for order_product in list(OrderList.objects.get(order=order).product)
+            ]
             total_order_price += order_product.price * order_product.quantity
         return JsonResponse({'order_list':order_list , 'total_order_price':total_order_price}, status=200)
     
