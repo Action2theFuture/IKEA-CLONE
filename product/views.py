75c78f2e092958e05b24b8bd964bc8d3cf3925b8
Action2theFuture <dnstks0204@google.com>
2021-05-18 16:50:11 +0900

    Refactoring

--- product/views.py
+++ product/views.py
@@ -1,31 +1,36 @@
 import json
 
 from django.views               import View
-from django.http                import JsonResponse, HttpResponse
+from django.http                import JsonResponse
+from django.core.exceptions     import ValidationError
 
 from product.models             import Product, SubCategory
 
 class ProductListView(View):
-    def get(self, request, english_name)
-        if SubCategory.objects.filter(english_name=sub_category_name).exists():
-            sub_category = SubCategory.objects.get(english_name=sub_category_name)
-            product_id   = Product.objects.get(sub_category=sub_category)
-            series       = product_id.series.english_name
-            products     = Product.objects.filter(sub_category=sub_category)
+    def get(self, request)
+        try:
+            pk = request.GET.get('id',None)
 
-            result = [{
-                        'korean_name'       : product.korean_name,
-                        'english_name'      : product.english_name,
-                        'price'             : product.price,
-                        'special_price'     : product.special_price,
-                        'is_new'            : product.is_new,
-                        'color_list'        : [color.name for color in product.color.all()],
-                        'sub_cat-egory_name': sub_category.korean_name,
-                        'image'             : Image.objects.get(product=product_id).url
-                        'series'            : series,
-                        'content'           : sub_category.content
-                    } for product in products]
+            if pk is not None:
+                sub_category = SubCategory.objects.get(id=pk)
+                series       = [series.english_name for series in product.series.all()]
+                products     = Product.objects.filter(sub_category=sub_category)
 
-            return JsonResponse({'result':result}, status=200)
-        return JsonResponse({'massage':'Non-existent SubCategoryName'}, status=404)
+                result = [{
+                            'korean_name'       : product.korean_name,
+                            'english_name'      : product.english_name,
+                            'price'             : product.price,
+                            'special_price'     : product.special_price,
+                            'is_new'            : product.is_new,
+                            'color_list'        : [color.name for color in product.color.all()],
+                            'sub_cat-egory_name': sub_category.korean_name,
+                            'image'             : [image.url for image in product.image.all()],
+                            'series'            : series,
+                            'content'           : sub_category.content
+                        } for product in products]
+
+                return JsonResponse({'result':result}, status=200)
+                
+        except ValidationError as e:
+            return JsonResponse({'massage':f'{e}'}, status=404)
 
