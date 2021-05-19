8578ecca4ead35151f5262265945fd623a36a9b7
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-19 16:56:38 +0900

    Refactor: NewListView

--- product/views.py
+++ product/views.py
@@ -6,20 +6,23 @@ from product.models import Product, BackgroundImage
 class NewListView(View):
     def get(self, request):
         new_products      = []
+        # background_image
         background_images = BackgroundImage.objects.all()
-        for background_image in background_images:
+        for background_image in background_images[1:]:
             new_products.append(
-                {
-                    'id'      : background_image.id,
-                    'src'     : background_image.url,
-                    'products': {
-                        'id'          : product.id,
-                        'is_new'      : product.is_new,
-                        'english_name': product.english_name,
-                        'korean_name' : product.korean_name,
-                        'price'       : product.price
-                    }   
-                }
-                for product in Product.objects.filter(background_image = background_image)
+                [
+                    {
+                        'id'      : background_image.id,
+                        'src'     : background_image.url,
+                        'products': {
+                            'id'          : product.id,
+                            'is_new'      : product.is_new,
+                            'english_name': product.english_name,
+                            'korean_name' : product.korean_name,
+                            'price'       : product.price
+                        }   
+                    }
+                    for product in Product.objects.filter(background_image = background_image)
+                ]
             )
         return JsonResponse({'new_products':new_products}, status=200)
