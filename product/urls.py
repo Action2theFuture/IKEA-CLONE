5d29c56a19bb1de6a8c51914fc8309fbd1b7d40c
Action2theFuture <dnstks0204@google.com>
2021-05-19 14:41:55 +0900

    commit

--- product/urls.py
+++ product/urls.py
@@ -3,5 +3,5 @@ from django.urls   import path
 from product.views import ProductListView
 
 urlpatterns = [
-    path('/list', ProductListView.as_view()),
+    path('/list/', ProductListView.as_view()),
 ]    
--- product/views.py
+++ product/views.py
@@ -10,7 +10,7 @@ from product.models               import Product, SubCategory
 from product.sub_product_queryset import get_queryset
 
 class ProductListView(View):
-    def get(self, request)
+    def get(self, request):
         try:
             sub_category_name = request.GET.get('sub_category_name',None)
             page              = request.GET.get('page',1)
@@ -21,7 +21,6 @@ class ProductListView(View):
             if sub_category_name is None:
                 return JsonResponse({'massage':'non-existent sub_Category_name key'}, status=404)
 
-            series       = [series.english_name for series in product.series.all()]
             products     = get_queryset(request)
 
             product_count = len(list(products))
@@ -58,11 +57,11 @@ class ProductListView(View):
                         'price'             : product.price,
                         'special_price'     : product.special_price,
                         'is_new'            : product.is_new,
-                        'color_list'        : [color.name for color in product.color.all()],
+                        #'color_list'        : [color.name for color in products.color.all()],
                         'sub_category_name' : sub_category.korean_name,
                         'sub_category_url'  : sub_category.english_name,
-                        'image'             : [image.url for image in product.image.all()],
-                        'series'            : series,
+                        'image'             : [image.url for image in products.image.all()],
+                        'series'            : [series.english_name for series in products.series.all()],
                         'content'           : sub_category.content,
                         'star'              : uniform(0.0,5.0)
                     } for product in products]
