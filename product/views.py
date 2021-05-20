2345f63ee3869ae6649c4d34bf846d90ac21a71e
Action2theFuture <dnstks0204@google.com>
2021-05-20 19:51:13 +0900

    commit

--- product/views.py
+++ product/views.py
@@ -29,7 +29,7 @@ class ProductListView(View):
             sort_list    = {'PRICE_LOW_TO_HIGH':'price',
                             'PRICE_HIGH_TO_LOW':'-price',
                                        'NEWEST':'is_new',
-                               'NAME_ASCENDING':Lower('korean_name')}
+                               'NAME_ASCENDING':Lower('english_name')}
             #정렬 기능
             if order_by in sort_list.keys():
                 if order_by == 'NEWEST':
