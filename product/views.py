518309fceb8bb386e63044188ab53590cfff62bd
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-20 12:55:28 +0900

    Refactor: JSON 구조 변환

--- product/views.py
+++ product/views.py
@@ -6,25 +6,21 @@ from product.models import Category, SubCategory
 class CategoryView(View):
     def get(self, request):
         category_list     = []
-        sub_category_list = {}
         categorys         = Category.objects.all()
 
         category_list = [
             {
                 'id'          : category.id,
                 'korean_name' : category.korean_name,
-                'english_name': category.english_name
-            }
-            for category in categorys
-        ]
-
-        for category in categorys:
-            sub_category_list[category.korean_name] = [
-                {
+                'english_name': category.english_name,
+                'sub_category': [
+                    {
                     'id'          : sub_category.id,
                     'korean_name' : sub_category.korean_name,
                     'english_name': sub_category.english_name
-                }
-                for sub_category in SubCategory.objects.filter(category=category)
-            ]
-        return JsonResponse({'category':category_list,'sub_category':sub_category_list}, status=200)
+                    }
+                for sub_category in SubCategory.objects.filter(category=category)]
+            }
+            for category in categorys]
+
+        return JsonResponse({'category':category_list}, status=200)
