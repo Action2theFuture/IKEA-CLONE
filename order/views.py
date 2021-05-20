a8e423957fa5fd3f0d0f72a93798b368a1ecee5a
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-20 14:24:36 +0900

    commit

--- order/views.py
+++ order/views.py
@@ -1,40 +1,44 @@
-import json
+from django.views     import View
+from django.http      import JsonResponse
+from django.db.models import F, Sum
 
-from django.views import View
-from django.http  import JsonResponse
 
-from order.models   import OrderList, Order
+from order.models   import OrderList
 from user.models    import User
 from user.utils     import authorize
 
 class OrderListView(View):
-    @authorize
+    #@authorize
     def get(self,request):
-        user          = request.user
-        order_list_id = request.GET.get('order_id', None)
-        quantity      = request.GET.get('quantity', 1)
-        if order_list_id and quantity:
-            order_product          = OrderList.objects.get(id=order_list_id)
-            order_product.quantity = quantity
+        #user          = request.user
+        user = User.objects.get(id=1)
+        # order_list_id = request.GET.get('order_id', None)
+        # quantity      = request.GET.get('quantity', 1)
+        # if order_list_id and quantity:
+        #     order_product          = OrderList.objects.get(id=order_list_id)
+        #     order_product.quantity = quantity
         
-        orders = list(user.order)
+        orders            = user.order.all()
         order_list        = [OrderList.objects.get(order=order) for order in orders]
         order_products    = []
         total_order_price = 0
-        for order in order_list: 
-            order_products = OrderList.objects.get(order=order).product.all()
-            order_products = [
-                {
-                    'id'          : order_product.id,
-                    'english_name': order_product.english_name,
-                    'korean_name' : order_product.korean_name,
-                    'sub_category': order_product.sub_category,
-                    'price'       : order_product.price,
-                    'url'         : order_product.url[0]
-                }
-                for order_product in order_products
-            ]
 
-            total_order_price += order_product.price * order_product.quantity
+        for order in order_list:
+            product = order.product
+            order_products.append(
+                    {
+                        'id'          : product.id,
+                        'english_name': product.english_name,
+                        'korean_name' : product.korean_name,
+                        'sub_category': product.sub_category.korean_name,
+                        'price'       : product.price,
+                        'url'         : product.image.all().first().url
+                    }
+                )
+            order.price = F('quantity') * product.price
+            total_order_price += order.price
+        print(total_order_price)
+        # order_list.aggregate(Sum('price'))
+        
         return JsonResponse({'order_list':order_products , 'total_order_price':total_order_price}, status=200)
     
