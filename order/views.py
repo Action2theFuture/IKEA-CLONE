0e1dd0b5a764857c37ae72fa74204530df68bb46
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-20 17:36:36 +0900

    Refactor: fetch 추가

--- order/views.py
+++ order/views.py
@@ -3,23 +3,15 @@ from django.http      import JsonResponse
 from django.db.models import F, Sum
 
 
-from order.models   import OrderList, Order, User
+from order.models   import OrderList, User
 from user.utils     import authorize
 
 class OrderListView(View):
     @authorize
     def get(self,request):
-        user          = request.user
-        order_list_id = request.GET.get('order_id', None)
-        quantity      = request.GET.get('quantity', 1)
-
-        if order_list_id and quantity:
-            order_product          = OrderList.objects.get(id
-            =order_list_id)
-            order_product.quantity = quantity
-
-        orders            = user.order.all()
-        order_list        = OrderList.objects.filter(order__in=orders)
+        user       = request.user
+        orders     = user.order.all()
+        order_list = OrderList.objects.filter(order__in=orders)
 
         order_products = [
             {
@@ -36,4 +28,10 @@ class OrderListView(View):
         total_order_price = order_list_qs.aggregate(total_price=Sum('price'))['total_price']
 
         return JsonResponse({'order_list':order_products , 'total_order_price':total_order_price}, status=200)
-    
+
+    def fetch(self, request):
+        order_list_id = request.GET.get('order_id', None)
+        quantity      = request.GET.get('quantity', 1)
+
+        order_product          = OrderList.objects.get(id=order_list_id)
+        order_product.quantity = quantity
