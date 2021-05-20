7d28a90a84c2236994def1b823982aa2b523568a
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-20 12:47:46 +0900

    Refactor: stock을 기준으로 추천제품 선정

--- product/views.py
+++ product/views.py
@@ -1,30 +1,27 @@
-from random import randrange, uniform
+from random import uniform
 
 from django.views           import View
 from django.http            import JsonResponse
 
-from product.models import Category
+from product.models import Product
 
 class RecommendedView(View):
     def get(self, request):
-        recommended_product  = []
-        category_list      = Category.objects.all()
-        random_number      = randrange(0,len(category_list))
-        recommended_category = category_list[random_number]
-        sub_categorys      = recommended_category.sub_category.all()
+        RECOMMENDED_COUNT = 10
+        products = Product.objects.all().order_by('-stock')[:RECOMMENDED_COUNT]
+        
         recommended_product= [
                 {
                     'is_new'           : product.is_new,
                     'english_name'     : product.english_name,
                     'korean_name'      : product.korean_name,
                     'price'            : product.price,
-                    'sub_category_name': sub_category.korean_name,
-                    'star'            : uniform(1.0,5.0),
+                    'sub_category_name': product.sub_category.korean_name,
+                    'star'             : uniform(1.0,5.0),
                     'image'            : [
                             image.url
-                            for image in product.image.all()[:2]
-                        ]
+                            for image in product.image.all()[:2]]
                 }
-                for sub_category in sub_categorys for product in list(sub_category.product.all())]
+                for product in products]
         
         return JsonResponse({'recommended_product':recommended_product}, status=200)
