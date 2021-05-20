9447f1f84db26d1bc16611fce145ed833f2ff68a
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-20 16:52:46 +0900

    Refactor: total_price aggregate로 구현

--- order/views.py
+++ order/views.py
@@ -3,8 +3,7 @@ from django.http      import JsonResponse
 from django.db.models import F, Sum
 
 
-from order.models   import OrderList
-from user.models    import User
+from order.models   import OrderList, Order, User
 from user.utils     import authorize
 
 class OrderListView(View):
@@ -13,28 +12,28 @@ class OrderListView(View):
         user          = request.user
         order_list_id = request.GET.get('order_id', None)
         quantity      = request.GET.get('quantity', 1)
-        if order_list_id and quantity:
-            order_product          = OrderList.objects.get(id=order_list_id)
-            order_product.quantity = quantity
-        
-        orders            = user.order.all()
-        order_list        = [OrderList.objects.get(order=order) for order in orders]
-        order_products    = []
-        total_order_price = 0
 
-        for order in order_list:
-            product = order.product
-            order_products.append(
-                    {
-                        'id'          : product.id,
-                        'english_name': product.english_name,
-                        'korean_name' : product.korean_name,
-                        'sub_category': product.sub_category.korean_name,
-                        'price'       : product.price,
-                        'url'         : product.image.all().first().url
-                    }
-                )
-            total_order_price += product.price * order.quantity
-        
+        if order_list_id and quantity:
+            order_product          = OrderList.objects.get(id
+            =order_list_id)
+            order_product.quantity = quantity
+
+        orders            = user.order.all()
+        order_list        = OrderList.objects.filter(order__in=orders)
+
+        order_products = [
+            {
+                'id'          : order.product.id,
+                'english_name': order.product.english_name,
+                'korean_name' : order.product.korean_name,
+                'sub_category': order.product.sub_category.korean_name,
+                'price'       : order.product.price,
+                'url'         : order.product.image.all().first().url
+            }
+            for order in order_list]
+
+        order_list_qs     = order_list.annotate(price=F('product__price')*F('quantity'))
+        total_order_price = order_list_qs.aggregate(total_price=Sum('price'))['total_price']
+
         return JsonResponse({'order_list':order_products , 'total_order_price':total_order_price}, status=200)
     
