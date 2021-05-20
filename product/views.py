448fc62bc72dcc78d58249bd682cd43a15de5d9a
Action2theFuture <dnstks0204@google.com>
2021-05-20 11:56:26 +0900

    Fix : 수정사항 반영

--- product/views.py
+++ product/views.py
@@ -3,32 +3,34 @@ from random                     import uniform
 
 from django.views               import View
 from django.http                import JsonResponse
+from django.core.exceptions     import ValidationError
 
 from product.models             import Product
 
 class ProductDetailView(View):
     def get(self ,request, product_id):
-        if Product.objects.filter(id=product_id).exists():
-            product      = Product.objects.get(id=product_id)
-            descriptions = product.description.values()
-            images_url   = [url['url'] for url in product.image.values('url')]
+        if not Product.objects.filter(id=product_id).exists():
+            return JsonResponse({'massage':'non-existent product'}, status=404)
+        product      = Product.objects.get(id=product_id)
+        descriptions = product.description.values()
+        images_url   = [url['url'] for url in product.image.values('url')]
 
-            result = [
-                {
-                'id'            : product.id,
-                'korean_name'   : product.korean_name,
-                'english_name'  : product.english_name,
-                'price'         : int(product.price),
-                'stock'         : product.stock,
-                'is_new'        : product.is_new,
-                'url'           : [image.url for image in product.image.all()],
-                'descriptions'  : list(descriptions),
-                'star'          : uniform(0.0,5.0),
-                'breadcrumb'    : [
-                    product.sub_category.category.korean_name, 
-                    {'id':product.sub_category.id, 
-                    'name':product.sub_category.korean_name}, 
-                    product.korean_name]
-                }]
+        result = [
+            {
+            'id'            : product.id,
+            'korean_name'   : product.korean_name,
+            'english_name'  : product.english_name,
+            'price'         : int(product.price),
+            'stock'         : product.stock,
+            'is_new'        : product.is_new,
+            'url'           : [image.url for image in product.image.all()],
+            'descriptions'  : list(descriptions),
+            'star'          : uniform(0.0,5.0),
+            'breadcrumb'    : [
+                product.sub_category.category.korean_name, 
+                {'id':product.sub_category.id, 
+                'name':product.sub_category.korean_name}, 
+                product.korean_name]
+            }]
 
         return JsonResponse({'product': result}, status=200)
